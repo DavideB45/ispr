ba5f5d41d20d299a5dc78116259ccd004ebdd1aa
DavideB45 <dadewww@gmail.com>
2024-03-10 22:48:50 +0100

    Update show_segmentation_sample.py

--- show_segmentation_sample.py
+++ show_segmentation_sample.py
@@ -1,12 +1,13 @@
 import cv2
+from typing import Tuple
 
 # Function to show a sample of the segmentation dataset
 def show_segmentation_sample(sampleID:int)->None:
+    
     # Load the first image
     image = cv2.imread('weizmann_horse_db/horse/horse'+str(sampleID).zfill(3)+'.png')
     mask = cv2.imread('weizmann_horse_db/mask/horse'+str(sampleID).zfill(3)+'.png', cv2.IMREAD_GRAYSCALE)
 
-    #print all pixels of the mask
     for i in range(mask.shape[0]):
         for j in range(mask.shape[1]):
             if mask[i][j] != 0:
@@ -16,10 +17,23 @@ def show_segmentation_sample(sampleID:int)->None:
 
     # Merge the image and the mask
     result = cv2.addWeighted(image, 0.9, cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR), 0.9, 0.5)
-    # Show the result
     cv2.imshow('Result', result)
     cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def show_image(imageID:int)->None:
+    image = cv2.imread('weizmann_horse_db/horse/horse'+str(imageID).zfill(3)+'.png')
+    cv2.imshow('Image', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def get_image(imageID:int)->Tuple[object, object]:
+    image = cv2.imread('weizmann_horse_db/horse/horse'+str(imageID).zfill(3)+'.png')
+    mask = cv2.imread('weizmann_horse_db/mask/horse'+str(imageID).zfill(3)+'.png', cv2.IMREAD_GRAYSCALE)
+    return image, mask
 
 if __name__ == '__main__':
+    show_image(1)
     show_segmentation_sample(1)
-    show_segmentation_sample(50)
+    show_image(100)
+    show_segmentation_sample(100)
