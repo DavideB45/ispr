60e2b7787e75661192f22ac7ea6a3e51e59a08e1
DavideB45 <dadewww@gmail.com>
2024-04-10 22:21:08 +0200

    CPT completed

--- secondAssignement/BaesianNetwork.py
+++ secondAssignement/BaesianNetwork.py
@@ -9,26 +9,66 @@
 
 
 class CPT:
+    '''
+    A Fancy Hash table
+    '''
     # should allow quick access to the probability of a certain value given the values of the parents
     # also if parents are not all observed, should return something that make sense (i.e. a probability distribution)
     # alternatively, throw an error
     # alernatively return a CPT resticted to the unobserved parents
     # is this just a big matrix?
-    #take a list of touple: [(name1, max1), (name2, max2), ...]
+    #take a list of touple: [(id, max1), (id, max2), ...]
     def __init__(self, conditioners):
-        self.conditionersNameOrder = [name for name, _ in conditioners]
+        self.conditionersIdOrder = [name for name, _ in conditioners]
         self.table = {}
         self.keys = ['']
         for name, max in conditioners:
+            tempKeys = []
             for i in range(max):
-                self.keys = [f'{key}{name}{i}' for key in self.keys]
-                
-                
-            
+                tempKeys += [f'{key}{name}{i}' for key in self.keys]
+            self.keys = tempKeys
 
-    def checkComplete(self):
-        # check if the table is complete
-        pass
+    def _getKeyFromQuery(self, valuedParents:dict[str:int], safeCreation:bool=False)->str:
+        '''Get the key from the query'''
+        key = '' 
+        for conditioner in self.conditionersIdOrder:
+            try:
+                key += f'{conditioner}{valuedParents[conditioner]}'
+            except KeyError:
+                raise('Not all parents ID are present')
+        if safeCreation and (key not in self.keys):
+            raise('Key not in the table')
+        return key
+
+    def checkComplete(self)->bool:
+        '''Check if the table is complete (i.e. all possible values are defined)'''
+        return len(self.keys) == len(self.table)
+
+    def setDistribution(self, valuedParents:dict[str:int], distribution:Distribution)->None:
+        '''Set the distribution for a certain set of parents
+        
+        valuedParents: dict (id, value) for the parents that are observed
+        distribution: the distribution to use when the parents are observed
+        
+        raises 'Not all parents are present' if not all parents are present
+        raises 'Key not in the table' if the key is not in the table
+        '''
+        key = self._getKeyFromQuery(valuedParents)
+        self.table[key] = distribution
+
+    def getDistribution(self, valuedParents:dict[str:int])->Distribution:
+        '''Get the distribution for a certain set of parents
+        
+        valuedParents: dictionary (id, value) for the parents that are observed
+
+        raises 'Table is not complete' if the table is not complete
+        raises 'Not all parents are present' if not all parents are present
+        raises 'Key not in the table' if the key is not in the table
+        '''
+        if not self.checkComplete():
+            raise('Table is not complete')
+        key = self._getKeyFromQuery(valuedParents)
+        return self.table[key]
 
 class Node:
     def __init__(self, name:str, id:str, parents:list, cpt:CPT):
