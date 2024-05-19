64da7d8e0e3e5705ac77deaf6ba772b1dcb9f6f6
DavideB45 <dadewww@gmail.com>
2024-05-19 13:33:55 +0200

    evaluation, but commented
    now it create

--- thirdAssignement/evaluateModel.py
+++ thirdAssignement/evaluateModel.py
@@ -4,22 +4,40 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from thirdAssignement.denoisingMLP import Autoencoder
 from thirdAssignement.utilities import load_mnist
+from sklearn.metrics import f1_score
 
-path = './thirdAssignement/model'
+path = './thirdAssignement/models'
 files = os.listdir(path)
 print(files)
-exit()
+files.remove('train.txt')
+device = torch.device('cuda' if torch.cuda.is_available() 
+                          else 'mps' if torch.backends.mps.is_available()
+                          else 'cpu')
 
+path = './thirdAssignement/models/DAE_0.5.pth'
+#for file in files:
 model = Autoencoder()
+#model.load_state_dict(torch.load(path + '/' + file))
 model.load_state_dict(torch.load(path))
+
+model.to(device)
 
 _, _, x_test = load_mnist()
 x_test = x_test[10:30]
+x_test = x_test.to(device)
 x_test = x_test.view(-1,1,28,28)
-x_test_noisy = x_test + 0.5 * torch.randn(x_test.size())
+x_test_noisy = 0.0*x_test + 1.0 * torch.randn(x_test.size(), device=device)
 x_test_noisy = torch.clamp(x_test_noisy, 0., 1.)
 with torch.no_grad():
-    x_reconstructed = model(x_test_noisy)
+    x_reconstructed = model(model(((model(model(x_test_noisy))))))
+
+#    original = x_test.cpu().numpy().reshape(-1)
+#    reconstructed = x_reconstructed.cpu().numpy().reshape(-1)
+    # make boolean array
+#    original = original > 0.5
+#    reconstructed = reconstructed > 0.5
+#    f_1 = f1_score(original, reconstructed, average='macro')
+#    print(f'model: {file}\tf1 score: {f_1}')
 
 import matplotlib.pyplot as plt
 n = 10
